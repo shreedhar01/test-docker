d9b89736e844c13eb5f47a4193a72fcf69352909
shreedhar01 <shreedharkalamagar@gmail.com>
2025-09-22 03:53:44 +0000

    feat: workflow_dispatch in docker-publish

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 import http from "http";
 const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
-    res.end("Hello from Node.js inside Docker \n");
+    res.end("Hello from Node.js inside Docker");
 });
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
