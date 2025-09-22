1ef0b46f2b8630692b879ba2b0c3c67aedb152c6
shreedhar01 <shreedharkalamagar@gmail.com>
2025-09-22 03:22:45 +0000

    feat: pre-push hook

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import http from "http"
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Node.js inside Docker 🚀\n");
+  res.end("Hello from Node.js inside Docker");
 });
 
 server.listen(port, () => {
